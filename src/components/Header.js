88383c9411233db3bf39dfbca46682b6cdb305de
amarmanhala <amar.manhala@gmail.com>
2023-04-25 22:37:29 -0400

    Add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { MdOutlineMenu, MdPerson2 } from "react-icons/md";
-import logo from "../logo.png";
+import logo from "../logo_dark.png";
 import Search from "./Search";
 
 const Header = () => {
   return (
     <header className="flex flex-row justify-between items-center w-full h-14 py-4 px-6 bg-[#0f0f0f]">
       <div className="flex flex-row justify-between items-center">
-        <div className="mr-4">
-          <MdOutlineMenu size={24} color="#fff" />
+        <div className="flex mr-4">
+          <button className="rounded-full"><MdOutlineMenu size={24} color="#fff" /></button>
         </div>
         <div>
           <img alt="youtube logo" src={logo} className="h-[20px] w-[90px]" />
         </div>
       </div>
-      <div className="w-2/6">
+      <div className="w-[40%]">
         <Search />
       </div>
       <div>
