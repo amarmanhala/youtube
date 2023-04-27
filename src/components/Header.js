91ddba6ce66ee10a4c16706efea6b8ae2eccedab
amarmanhala <amar.manhala@gmail.com>
2023-04-27 17:28:42 -0400

    Add search suggestions

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import { YOUTUBE_SEARCH_SUGGESTION_API } from "../utils/config";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
   //console.log(searchQuery);
   const dispatch = useDispatch();
 
@@ -16,21 +17,22 @@ const Header = () => {
 
     return () => {
       clearTimeout(timer);
-    }
+    };
   }, [searchQuery]);
 
   const getSearchSuggestions = async () => {
-    console.log("Call an api with", searchQuery)
+    console.log("Call an api with", searchQuery);
     const data = await fetch(YOUTUBE_SEARCH_SUGGESTION_API + searchQuery);
     const json = await data.json();
+    setSearchSuggestions(json[1]);
     //console.log(json[1]);
-  }
+  };
 
   const toggleSidebar = () => {
     dispatch(toggleActionForSidebar());
   };
   return (
-    <header className="flex flex-row justify-between items-center w-full h-14 py-4 px-6 bg-zinc-950">
+    <header className="flex flex-row justify-between  w-full h-14 py-4 px-6 bg-zinc-950">
       <div className="flex flex-row justify-between items-center">
         <div className="flex mr-4">
           <button onClick={() => toggleSidebar()} className="rounded-full">
@@ -41,9 +43,25 @@ const Header = () => {
           <img alt="youtube logo" src={logo} className="h-[20px] w-[90px]" />
         </div>
       </div>
-      <div className="w-[40%]">
-        <Search value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+      <div className="flex flex-col w-[40%]">
+        <div className="w-full">
+          <Search
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        <div className="bg-white border border-zinc-200 relative w-full rounded-xl mt-1">
+
+         <div className="py-6 px-4">
+            <ul className="text-lg font-medium">
+             {searchSuggestions.map((suggestion) => {
+              return (<li key={suggestion} className="pb-2">{suggestion}</li>)
+             })}
+            </ul>
+         </div>
+        </div>
       </div>
+
       <div>
         <MdPerson2 size={24} color="#fff" />
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,7 @@ const Search = ({ value, onChange }) => {
     <div className="flex flex-row justify-center w-full">
       <input value={value} onChange={onChange} type='text' placeholder='Search' className="w-full px-4 py-2 bg-transparent text-white placeholder:text-gray-500 border border-[#303030] border-r-0 rounded-l-full font-normal text-sm outline-none" />
       <button className="bg-[#ffffff14] px-4 border border-[#303030] rounded-r-full"><MdOutlineSearch size={24} color="#fff" /></button>
+      
     </div>
   )
 }
