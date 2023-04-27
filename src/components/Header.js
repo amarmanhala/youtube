f7d1de412db3f14ce45d930b3fcc93d883717876
amarmanhala <amar.manhala@gmail.com>
2023-04-27 16:38:14 -0400

    Add debouncing to search

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,27 @@ import logo from "../logo_dark.png";
 import Search from "./Search";
 import { useDispatch } from "react-redux";
 import { toggleActionForSidebar } from "../utils/sidebarToggleSlice";
+import { YOUTUBE_SEARCH_SUGGESTION_API } from "../utils/config";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  console.log(searchQuery);
+  //console.log(searchQuery);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
 
+    return () => {
+      clearTimeout(timer);
+    }
   }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    console.log("Call an api with", searchQuery)
+    const data = await fetch(YOUTUBE_SEARCH_SUGGESTION_API + searchQuery);
+    const json = await data.json();
+    //console.log(json[1]);
+  }
 
   const toggleSidebar = () => {
     dispatch(toggleActionForSidebar());
