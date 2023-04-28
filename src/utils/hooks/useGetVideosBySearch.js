dcb65bd4a405f10e75e791f4ef17ed7ab0d6e83a
amarmanhala <amar.manhala@gmail.com>
2023-04-28 18:48:24 -0400

    Add search query

--- src/utils/hooks/useGetVideosBySearch.js
+++ src/utils/hooks/useGetVideosBySearch.js
@@ -1,9 +1,23 @@
 import { useSelector } from "react-redux";
+import { GOOGLE_API_KEY, YOUTUBE_BASE_SEARCH_URL } from "../config";
+import { useEffect, useState } from "react";
 
 const useGetVideosBySearch = (searchQuery) => {
-  
-  
+  const [videos, setVideos] = useState([]);
   console.log(searchQuery);
+
+  useEffect(() => {
+    getVideosBySearch(searchQuery);
+    console.log(videos);
+  }, []);
+
+  const getVideosBySearch = async (searchQuery) => {
+    const data = await fetch(
+      `${YOUTUBE_BASE_SEARCH_URL}&q=${searchQuery}&maxResults=${25}`
+    );
+    const json = await data.json();
+    setVideos(json);
+  };
 };
 
 export default useGetVideosBySearch;
