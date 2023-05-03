a78940a281b7d3d7fcffedc891e62e9b047be676
amarmanhala <amar.manhala@gmail.com>
2023-05-03 18:07:13 -0400

    Fix

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -30,15 +30,28 @@ const LiveChat = () => {
       <div className="py-4">
         <h2 className="text-white">Live Chat</h2>
       </div>
-    <div className="flex w-full overflow-y-scroll flex-col flex-col-reverse">
+      <div className="flex w-full overflow-y-scroll flex-col flex-col-reverse">
         {chatMessages.map((chat, index) => {
           return (
             <ChatMessage key={index} name={chat.name} message={chat.message} />
           );
         })}
-        
-    </div>
-    <div className="w-full"><input value={message} onChange={(e) => setMessage(e.target.value)} placeholder="type something..." className="w-full border border-blue-600 bg-transparent p-2 outline-none"></input></div>
+      </div>
+      <form className="w-full" onSubmit={(e) => {
+        e.preventDefault();
+        dispatch(addMessage({
+          name: "Amar",
+          message: message
+        }));
+        setMessage("");
+      }}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="type something..."
+          className="text-white w-full border border-blue-600 bg-transparent p-2 outline-none"
+        ></input>
+      </form>
     </div>
   );
 };
