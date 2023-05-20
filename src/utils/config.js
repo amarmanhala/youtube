2c764f5f4e53167567f00b73cba486188eb7c749
amarmanhala <amar.manhala@gmail.com>
2023-05-20 12:27:13 -0400

    Fix

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+const MAX_RESULTS = 40;
+
+export const YOUTUBE_POPULAR_VIDEO_LIST = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key=" + process.env.REACT_APP_API_KEY + "&maxResults=" + MAX_RESULTS;
+
+export const YOUTUBE_SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const YOUTUBE_BASE_SEARCH_URL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=" + process.env.REACT_APP_API_KEY;
+
+export const YOUTUBE_CHANNEL_LIST = "https://youtube.googleapis.com/youtube/v3/channels?part=snippet&key=" + process.env.REACT_APP_API_KEY;
+
